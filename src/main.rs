fa86f9368790017f9624303671aaf072028238c9
eduardojmolinos <eduardo-jose.molinos@renault.com>
2025-04-22 11:57:06 +0000

    Exercise completed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
 mod run_length_encoding {
     pub fn encode(text: &str) -> String {
-        todo!()
+        let mut count = 1;
+        let mut letter = text.chars().next().unwrap();
+        let mut str = String::new();
+        println!("{}", letter);
+        for i in text.chars().skip(1) {
+            println!("{}", i);
+            if i == letter {
+                count = count + 1;
+                if count == 10 {
+                    str.push_str(&format!("{}{}", count - 1, letter));
+                    count = 1;
+                }
+            } else {
+                str.push_str(&format!("{}{}", count, letter));
+                println!("{}", letter);
+                letter = i;
+                count = 1;
+            }
+        }
+        str.push_str(&format!("{}{}", count, letter));
+        println!("{}", str);
+        return str;
     }
-    
+
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut str = String::new();
+        let mut count = 0;
+        let mut rep = 0;
+        for i in text.chars() {
+            if count % 2 == 0 {
+                rep = u32::from(i) - 48;
+                println!("{}{}", rep, i);
+                //str.push_str(&format!("{}", i));
+            } else {
+                let mut j = 0;
+                while j < rep {
+                    str.push_str(&format!("{}", i));
+                    j = j + 1;
+                }
+            }
+            count = count + 1;
+        }
+        return str;
     }
 }
 
 fn main() {
-    // 
+    //
 }
 
 #[test]
